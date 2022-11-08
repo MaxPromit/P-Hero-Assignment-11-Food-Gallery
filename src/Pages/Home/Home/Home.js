c5b8318a865dbacf9a9ebd33a10612530c088aac
max <promitms7@gmail.com>
2022-11-08 20:18:24 +0600

    home done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            this is home
+        <div className='flex justify-evenly items-center'>
+            <div>
+                <h1 className='text-8xl font-semibold'>
+                The Best <br />
+                Restaurants <br />
+                In Your Home
+                </h1>
+            </div>
+            <div>
+                <img src="https://i.ibb.co/mSn1G3x/home.png" alt="" />
+            </div>
         </div>
     );
 };
 
 export default Home;
+
