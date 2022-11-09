2c069a42d0a393965287c989c100effef64bdd9c
max <promitms7@gmail.com>
2022-11-09 06:49:07 +0600

    firebase config file secret

--- src/Pages/Home/Services/ServiceDetails.js
+++ src/Pages/Home/Services/ServiceDetails.js
@@ -5,12 +5,12 @@ const ServiceDetails = () => {
     const {title,price,details,img} = useLoaderData()
     return (
         <div>
-            <h2 className='text-2xl font-semibold text-center'>Service Details Section</h2>
+            <h2 className='text-3xl font-semibold text-center'>Service Details Section</h2>
             <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto mt-5">
         <figure><img src={img} alt="Shoes" /></figure>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
-          <p>{details.slice(0, 150) + '...'}</p>
+          <p>{details}</p>
           <div className="card-actions flex items-center justify-between">
            <div>
             <h3 className='text-2xl text-indigo-500 font-semibold'>Price: ${price}</h3>
