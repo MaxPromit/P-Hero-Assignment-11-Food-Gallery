28b5295c31d7f5f9cd3551359e5cd31dc5e64627
max <promitms7@gmail.com>
2022-11-10 13:28:31 +0600

    job done

--- src/Pages/Home/Services/AllCardServiceItems.js
+++ src/Pages/Home/Services/AllCardServiceItems.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
-const AllCardServiceItems = ({product}) => {
-  document.title = 'Food Gallery - Services';
-    const {img,price,title,details,_id} = product;
-    return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure><img src={img} alt="Shoes" /></figure>
-        <div className="card-body">
-          <h2 className="card-title">{title}</h2>
-          <p>{details}</p>
-          <div className="card-actions flex items-center justify-between">
-           <div>
-            <h3 className='text-2xl text-indigo-500 font-semibold'>Price: ${price}</h3>
-           </div>
-            <div>
-           <Link to={`/serviceDetails/${_id}`}>
-            <button className="btn btn-primary">View Details</button>
-            </Link> 
-            </div>
+const AllCardServiceItems = ({ product }) => {
+  document.title = "Food Gallery - Services";
+  const { img, price, title, details, _id } = product;
+  return (
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+       <PhotoProvider>
+       
+      <div className="foo">
+      
+          <PhotoView key={_id} src={img}>
+            <img src={img} alt="" style={{ objectFit: 'cover' }}/>
+          </PhotoView>
+        
+      </div>
+       
+    </PhotoProvider>
+
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{details}</p>
+        <div className="card-actions flex items-center justify-between">
+          <div>
+            <h3 className="text-2xl text-indigo-500 font-semibold">
+              Price: ${price}
+            </h3>
+          </div>
+          <div>
+            <Link to={`/serviceDetails/${_id}`}>
+              <button className="btn btn-primary">View Details</button>
+            </Link>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 export default AllCardServiceItems;
